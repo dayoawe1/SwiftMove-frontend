359d8934703b7bb91a549864f2c6f1f7bd06bd62
dayoawe1 <dayoawe1@yahoo.com>
2026-01-28 09:33:39 -0500

    add back services/index.js

---
+++ src/components/services/index.js
@@ -0,0 +1,5 @@
+export { ResidentialMovingPage } from './ResidentialMovingPage';
+export { CommercialMovingPage } from './CommercialMovingPage';
+export { MovingSupportPage } from './MovingSupportPage';
+export { ResidentialCleaningPage } from './ResidentialCleaningPage';
+export { CommercialCleaningPage } from './CommercialCleaningPage';
