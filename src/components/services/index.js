a47877c3a96b45883d6ff0c02638dbece09e3494
dayoawe1 <dayoawe1@yahoo.com>
2026-01-28 09:06:12 -0500

    remove services/index.js

--- src/components/services/index.js
+++
@@ -1,5 +0,0 @@
-export { ResidentialMovingPage } from './ResidentialMovingPage';
-export { CommercialMovingPage } from './CommercialMovingPage';
-export { MovingSupportPage } from './MovingSupportPage';
-export { ResidentialCleaningPage } from './ResidentialCleaningPage';
-export { CommercialCleaningPage } from './CommercialCleaningPage';
